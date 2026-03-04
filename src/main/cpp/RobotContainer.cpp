2e02c20d9dd548747dd10db25b54d8e375bd31b7
Commandrew1337 <apanning@gmail.com>
2026-03-04 16:39:50 -0600

    replaced button elevator climb command for secondary intake wheels

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -81,14 +81,8 @@ void RobotContainer::ConfigureBindings() {
   // Elevator Low
   m_buttons.Button(1).OnTrue(m_elevator.SetHeightSafedCmd(constants::Elevator::kMinHeight, m_intake));
 
-  //Elevator Climb
-  m_buttons.Button(2).OnTrue(
-    frc2::cmd::Parallel(
-      m_elevator.SetHeightSafedCmd(constants::Elevator::kClimbHeight, m_intake),
-      m_shooter.SetHoodDegCommand(30.0)  // one-shot: calls Shooter::SetHoodDeg(30)
-    )
-  );
-
+  //Secondary Intake In Wheels
+  m_buttons.Button(2).WhileTrue(m_intake.WheelsPercentCmd(constants::Intake::kOpenLoopIntake));
 
   //Elevator High
   m_buttons.Button(3).OnTrue(m_elevator.SetHeightSafedCmd(constants::Elevator::kMaxHeight, m_intake));
